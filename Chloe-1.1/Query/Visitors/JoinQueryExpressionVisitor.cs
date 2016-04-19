6d3bd9e1c04a7b967022d9f32407f15ae851ccaa
shuxinqin@outlook.com <shuxinqin@outlook.com>
2016-04-19 11:10:10 +0800

    .

--- Chloe-1.1/Query/Visitors/JoinQueryExpressionVisitor.cs
+++ Chloe-1.1/Query/Visitors/JoinQueryExpressionVisitor.cs
@@ -1,4 +1,7 @@
 ﻿using Chloe.DbExpressions;
+using Chloe.Descriptors;
+using Chloe.Query.QueryExpressions;
+using Chloe.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +10,53 @@ using System.Threading.Tasks;
 
 namespace Chloe.Query.Visitors
 {
-    class JoinQueryExpressionVisitor
+    class JoinQueryExpressionVisitor : QueryExpressionVisitor<JoinQueryResult>
     {
+        JoinType _joinType;
+        DbTableExpression _table;
+        DbTableExpression _relatedTable;
+        DbFromTableExpression _fromTable;
+        ResultElement _resultElement;
+        JoinQueryExpressionVisitor(JoinType joinType, DbTableExpression table, DbTableExpression relatedTable)
+        {
 
+        }
 
+        public override JoinQueryResult Visit(RootQueryExpression exp)
+        {
+            Type type = exp.ElementType;
+            MappingTypeDescriptor typeDescriptor = MappingTypeDescriptor.GetEntityDescriptor(type);
+
+            DbTableExpression tableExp = CreateTableExpression(typeDescriptor.TableName, this._resultElement.GenerateUniqueTableAlias(typeDescriptor.TableName));
+            MappingObjectExpression moe = new MappingObjectExpression(typeDescriptor.EntityType.GetConstructor(UtilConstants.EmptyTypeArray));
+
+            foreach (MappingMemberDescriptor item in typeDescriptor.MappingMemberDescriptors)
+            {
+                DbColumnAccessExpression columnAccessExpression = new DbColumnAccessExpression(item.MemberType, tableExp, item.ColumnName);
+                moe.AddMemberExpression(item.MemberInfo, columnAccessExpression);
+            }
+
+            //TODO 解析 on 条件表达式
+            //var visitor = new GeneralExpressionVisitor(moe);
+            //visitor
+
+            DbExpression condition = null;
+
+            DbJoinTableExpression joinTable = new DbJoinTableExpression(this._joinType, tableExp, this._relatedTable, this._fromTable, condition);
+
+            JoinQueryResult result = new JoinQueryResult();
+            result.MappingObjectExpression = moe;
+            result.JoinTable = joinTable;
+
+            return result;
+        }
+
+        static DbTableExpression CreateTableExpression(string tableName, string alias)
+        {
+            DbDerivedTableExpression rootTable = new DbDerivedTableExpression(tableName);
+            DbTableExpression tableExp = new DbTableExpression(rootTable, alias);
+            return tableExp;
+        }
     }
 
     class JoinQueryResult
---
+++ Chloe/Query/QueryExpressions/MinExpression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chloe.Query.QueryExpressions
+{
+    public class MinExpression : SingleParamFnQueryExpression
+    {
+        public MinExpression(QueryExpression prevExpression, Expression selector)
+            : base(QueryExpressionType.Min, prevExpression, selector)
+        {
+        }
+    }
+}
