a1304a88a867a6c57f3914118aa0493b490ebd29
shuxinqin@outlook.com <shuxinqin@outlook.com>
2016-07-10 18:05:31 +0800

    add interface 'IDbConnectionFactory'

---
+++ Chloe/Infrastructure/IDbConnectionFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Chloe.Infrastructure
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection CreateConnection();
+    }
+}
