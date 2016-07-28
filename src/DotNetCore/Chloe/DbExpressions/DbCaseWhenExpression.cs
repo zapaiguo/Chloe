cd6c0c563a13b3e5b4a9da9d96ac719d3568e270
so <shuxinqin@outlook.com>
2016-07-29 01:21:37 +0800

    Init chloe.dll for .net core.

---
+++ src/DotNetCore/Chloe/DbExpressions/DbCaseWhenExpression.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Chloe.DbExpressions
+{
+    public class DbCaseWhenExpression : DbExpression
+    {
+        ReadOnlyCollection<WhenThenExpressionPair> _whenThenExps;
+        DbExpression _elseExp;
+        public DbCaseWhenExpression(Type type, IList<WhenThenExpressionPair> whenThenExps, DbExpression elseExp)
+            : base(DbExpressionType.CaseWhen, type)
+        {
+            this._whenThenExps = new ReadOnlyCollection<WhenThenExpressionPair>(whenThenExps);
+            this._elseExp = elseExp;
+        }
+
+        public ReadOnlyCollection<WhenThenExpressionPair> WhenThenExps { get { return this._whenThenExps; } }
+        public DbExpression Else { get { return this._elseExp; } }
+
+        public override T Accept<T>(DbExpressionVisitor<T> visitor)
+        {
+            return visitor.Visit(this);
+        }
+
+        public struct WhenThenExpressionPair
+        {
+            DbExpression _when;
+            DbExpression _then;
+            public WhenThenExpressionPair(DbExpression when, DbExpression then)
+            {
+                this._when = when;
+                this._then = then;
+            }
+
+            public DbExpression When { get { return this._when; } }
+            public DbExpression Then { get { return this._then; } }
+        }
+    }
+}
---
+++ src/DotNetCore/Chloe/DbExpressions/DbOrderSegment.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Chloe.DbExpressions
+{
+    public class DbOrderSegment
+    {
+        OrderType _orderType;
+        DbExpression _expression;
+        public DbOrderSegment(DbExpression expression, OrderType orderType)
+        {
+            this._expression = expression;
+            this._orderType = orderType;
+        }
+        public DbExpression DbExpression { get { return this._expression; } }
+        public OrderType OrderType { get { return this._orderType; } }
+    }
+}
