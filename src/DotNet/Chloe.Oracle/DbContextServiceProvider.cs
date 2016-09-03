b85c3b4a426913190620427abbc6f8417740cdcc
shuxinqin@outlook.com <shuxinqin@outlook.com>
2016-09-04 00:20:39 +0800

    Add Chloe.Oracle project.

---
+++ src/DotNet/Chloe.Oracle/DbContextServiceProvider.cs
@@ -0,0 +1,40 @@
+﻿using Chloe.Core.Visitors;
+using Chloe.Infrastructure;
+using Chloe.Query;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Chloe.Oracle
+{
+    class DbContextServiceProvider : IDbContextServiceProvider
+    {
+        IDbConnectionFactory _dbConnectionFactory;
+        OracleContext _oracleContext;
+
+        public DbContextServiceProvider(IDbConnectionFactory dbConnectionFactory, OracleContext oracleContext)
+        {
+            this._dbConnectionFactory = dbConnectionFactory;
+            this._oracleContext = oracleContext;
+        }
+        public IDbConnection CreateConnection()
+        {
+            return this._dbConnectionFactory.CreateConnection();
+        }
+        public IDbExpressionTranslator CreateDbExpressionTranslator()
+        {
+            if (this._oracleContext.ConvertToUppercase == true)
+            {
+                return DbExpressionTranslator_ConvertToUppercase.Instance;
+            }
+            else
+            {
+                return DbExpressionTranslator.Instance;
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+}
