3b27778d2971ac35d5473d534e3d6bd84d71503b
shuxinqin@outlook.com <shuxinqin@outlook.com>
2017-04-21 15:45:42 +0800

    remove unused files.

---
+++ src/DotNet/ChloeTest/PerformanceTest.cs
@@ -0,0 +1,19 @@
+﻿using Chloe.SqlServer;
+using Database;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Collections;
+using Chloe.Core;
+
+namespace ChloeTest
+{
+    public class PerformanceTest
+    {
+
+    }
+}
