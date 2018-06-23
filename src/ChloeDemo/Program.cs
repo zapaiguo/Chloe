a7ad09f62c9e2083ee2b593e903ba374764532e7
shuxinqin <shuxinqin@outlook.com>
2018-06-23 19:51:07 +0800

    .

--- src/ChloeDemo/Program.cs
+++ src/ChloeDemo/Program.cs
@@ -26,8 +26,6 @@ namespace ChloeDemo
             MySqlDemo.Run();
             PostgreSQLDemo.Run();
             OracleDemo.Run();
-
-            //RegisterMappingTypeDemo.RunDemo();
         }
     }
 }
