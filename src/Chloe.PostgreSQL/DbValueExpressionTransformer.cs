3cd8f67b62e22369ba08b7b9bacdfdcf7c5af6dc
shuxinqin <shuxinqin@outlook.com>
2019-01-27 22:29:27 +0800

    code fix.

--- src/Chloe.PostgreSQL/DbValueExpressionTransformer.cs
+++ src/Chloe.PostgreSQL/DbValueExpressionTransformer.cs
@@ -3,196 +3,189 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Chloe.PostgreSQL
 {
-    class DbValueExpressionVisitor : DbExpressionVisitor<DbExpression>
+    class DbValueExpressionTransformer : DbExpressionVisitor
     {
-        SqlGenerator _generator = null;
+        static DbValueExpressionTransformer _transformer = new DbValueExpressionTransformer();
 
-        public DbValueExpressionVisitor(SqlGenerator generator)
+        public static DbExpression Transform(DbExpression exp)
         {
-            if (generator == null)
-                throw new ArgumentNullException("generator");
+            return exp.Accept(_transformer);
+        }
 
-            this._generator = generator;
+        DbExpression ConvertDbBooleanExpression(DbExpression exp)
+        {
+            DbCaseWhenExpression caseWhenExpression = SqlGenerator.ConstructReturnCSharpBooleanCaseWhenExpression(exp);
+            return caseWhenExpression;
         }
 
         public override DbExpression Visit(DbEqualExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
         public override DbExpression Visit(DbNotEqualExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
         public override DbExpression Visit(DbNotExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
 
         public override DbExpression Visit(DbBitAndExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         public override DbExpression Visit(DbAndExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
         public override DbExpression Visit(DbBitOrExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         public override DbExpression Visit(DbOrExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
 
         public override DbExpression Visit(DbConvertExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         // +
         public override DbExpression Visit(DbAddExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         // -
         public override DbExpression Visit(DbSubtractExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         // *
         public override DbExpression Visit(DbMultiplyExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         // /
         public override DbExpression Visit(DbDivideExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         // %
         public override DbExpression Visit(DbModuloExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         public override DbExpression Visit(DbNegateExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         // <
         public override DbExpression Visit(DbLessThanExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
         // <=
         public override DbExpression Visit(DbLessThanOrEqualExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
         // >
         public override DbExpression Visit(DbGreaterThanExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
         // >=
         public override DbExpression Visit(DbGreaterThanOrEqualExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
 
         public override DbExpression Visit(DbConstantExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbCoalesceExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbCaseWhenExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbTableExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbColumnAccessExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbMemberExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         public override DbExpression Visit(DbParameterExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbSubQueryExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         public override DbExpression Visit(DbSqlQueryExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbMethodCallExpression exp)
         {
             if (exp.Type == UtilConstants.TypeOfBoolean || exp.Type == UtilConstants.TypeOfBoolean_Nullable)
-                return this.VisistDbBooleanExpression(exp);
+                return this.ConvertDbBooleanExpression(exp);
             else
-                return exp.Accept(this._generator);
+                return exp;
         }
 
         public override DbExpression Visit(DbFromTableExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbJoinTableExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         public override DbExpression Visit(DbAggregateExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbInsertExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         public override DbExpression Visit(DbUpdateExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
         public override DbExpression Visit(DbDeleteExpression exp)
         {
-            return exp.Accept(this._generator);
+            return exp;
         }
 
         public override DbExpression Visit(DbExistsExpression exp)
         {
-            return this.VisistDbBooleanExpression(exp);
+            return this.ConvertDbBooleanExpression(exp);
         }
-
-        DbExpression VisistDbBooleanExpression(DbExpression exp)
-        {
-            DbCaseWhenExpression caseWhenExpression = SqlGenerator.ConstructReturnCSharpBooleanCaseWhenExpression(exp);
-            this.Visit(caseWhenExpression);
-            return exp;
-        }
-
     }
-
 }
--- src/Chloe.PostgreSQL/SqlGenerator.cs
+++ src/Chloe.PostgreSQL/SqlGenerator.cs
@@ -20,8 +20,6 @@ namespace Chloe.PostgreSQL
         ISqlBuilder _sqlBuilder = new SqlBuilder();
         DbParamCollection _parameters = new DbParamCollection();
 
-        DbValueExpressionVisitor _valueExpressionVisitor;
-
         public static readonly Dictionary<string, IMethodHandler> MethodHandlers = GetMethodHandlers();
         static readonly Dictionary<string, Action<DbAggregateExpression, SqlGenerator>> AggregateHandlers = InitAggregateHandlers();
         static readonly Dictionary<MethodInfo, Action<DbBinaryExpression, SqlGenerator>> BinaryWithMethodHandlers = InitBinaryWithMethodHandlers();
@@ -73,17 +71,6 @@ namespace Chloe.PostgreSQL
 
         public ISqlBuilder SqlBuilder { get { return this._sqlBuilder; } }
         public List<DbParam> Parameters { get { return this._parameters.ToParameterList(); } }
-
-        DbValueExpressionVisitor ValueExpressionVisitor
-        {
-            get
-            {
-                if (this._valueExpressionVisitor == null)
-                    this._valueExpressionVisitor = new DbValueExpressionVisitor(this);
-
-                return this._valueExpressionVisitor;
-            }
-        }
 
         public static SqlGenerator CreateInstance()
         {
@@ -489,7 +476,7 @@ namespace Chloe.PostgreSQL
 
                 DbExpression valExp = DbExpressionExtension.StripInvalidConvert(item.Value);
                 AmendDbInfo(item.Key, valExp);
-                valExp.Accept(this.ValueExpressionVisitor);
+                DbValueExpressionTransformer.Transform(valExp).Accept(this);
                 separator = ",";
             }
 
@@ -528,7 +515,7 @@ namespace Chloe.PostgreSQL
 
                 DbExpression valExp = DbExpressionExtension.StripInvalidConvert(item.Value);
                 AmendDbInfo(item.Key, valExp);
-                valExp.Accept(this.ValueExpressionVisitor);
+                DbValueExpressionTransformer.Transform(valExp).Accept(this);
             }
 
             this.BuildWhereState(exp.Condition);
@@ -813,7 +800,7 @@ namespace Chloe.PostgreSQL
         }
         internal void AppendColumnSegment(DbColumnSegment seg)
         {
-            seg.Body.Accept(this.ValueExpressionVisitor);
+            DbValueExpressionTransformer.Transform(seg.Body).Accept(this);
             this._sqlBuilder.Append(" AS ");
             this.QuoteName(seg.Alias);
         }
