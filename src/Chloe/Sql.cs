e1ee971ad1360f28eb4640663e2627e0ac196ca7
shuxinqin <shuxinqin@outlook.com>
2020-04-12 20:18:33 +0800

    .

--- src/Chloe/Sql.cs
+++ src/Chloe/Sql.cs
@@ -18,7 +18,7 @@ namespace Chloe
         }
         /// <summary>
         /// 比较两个相同类型的对象值是否不相等，仅支持在 lambda 表达式树中使用。
-        /// 使用此方法与不等号（!=）的区别是：a => Sql.NotEquals(a.Name, a.XName) 会被翻译成 a.Name &lt;&gt; a.XName，而 a => a.Name != a.XName 则会被翻译成 a.Name &lt;&gt; a.XName or (a.Name is null and a.XName is not null) or (a.Name is not null and a.XName is null)
+        /// 使用此方法与不等号（!=）的区别是：a => Sql.NotEquals(a.Name, a.XName) 会被翻译成 a.Name != a.XName，而 a => a.Name != a.XName 则会被翻译成 a.Name != a.XName or (a.Name is null and a.XName is not null) or (a.Name is not null and a.XName is null)
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="value1"></param>
